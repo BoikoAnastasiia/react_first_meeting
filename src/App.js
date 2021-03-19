b23d5ef06eb67c243510ec18595c64b3a1bcec16
anastasyah86 <nastasyah86@gmail.com>
2021-03-19 17:54:41 +0200

    thanks mitya

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>The main component is an App container</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -20,29 +23,29 @@ import ReactDOM from "react-dom";
 
 // ReactDOM.render(link, document.getElementById("root"));
 
-const paintings = {
-  id: "id-1",
-  url:
-    "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
-  title: "Feathers. Art abstract",
-  price: 300,
-  author: {
-    tag: "ractapopulous",
-    url: "https://pixabay.com/users/ractapopulous-24766/",
-  },
-  quantity: 10,
-};
+// const paintings = {
+//   id: "id-1",
+//   url:
+//     "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+//   title: "Feathers. Art abstract",
+//   price: 300,
+//   author: {
+//     tag: "ractapopulous",
+//     url: "https://pixabay.com/users/ractapopulous-24766/",
+//   },
+//   quantity: 10,
+// };
 
-const template = (
-  <div>
-    <img src={paintings.url} alt={paintings.title} width="480" />
-    <h2>{paintings.title}</h2>
-    <p>
-      Author: <a href={paintings.author.url}>{paintings.author.tag}</a>
-    </p>
-    <p>Price: {paintings.price} credits</p>
-    <button type="button">Add to basket </button>
-  </div>
-);
-console.log(template);
-ReactDOM.render(template, document.getElementById("root"));
+// const template = (
+//   <div>
+//     <img src={paintings.url} alt={paintings.title} width="480" />
+//     <h2>{paintings.title}</h2>
+//     <p>
+//       Author: <a href={paintings.author.url}>{paintings.author.tag}</a>
+//     </p>
+//     <p>Price: {paintings.price} credits</p>
+//     <button type="button">Add to basket </button>
+//   </div>
+// );
+// console.log(template);
+// ReactDOM.render(template, document.getElementById("root"));
