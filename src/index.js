467e5f31c69ee1bcec1932798e71d9fec686f3a1
anastasyah86 <nastasyah86@gmail.com>
2021-03-19 17:07:31 +0200

    basic prps

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+// const link = React.createElement(
+//   "a",
+//   { href: "https://www.react.org/", target: "_blank" },
+//   "The Link"
+// );
+
+// const jsxLink = <a href="https://www.react.org/"> "The Link"</a>;
+
+// console.log(link);
+// console.log(jsxLink);
+
+// ReactDOM.render(link, document.getElementById("root"));
+
+const paintings = {
+  id: "id-1",
+  url:
+    "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+  title: "Feathers. Art abstract",
+  price: 300,
+  author: {
+    tag: "ractapopulous",
+    url: "https://pixabay.com/users/ractapopulous-24766/",
+  },
+  quantity: 10,
+};
+
+const template = (
+  <div>
+    <img src={paintings.url} alt={paintings.title} width="480" />
+    <h2>{paintings.title}</h2>
+    <p>
+      Author: <a href={paintings.author.url}>{paintings.author.tag}</a>
+    </p>
+    <p>Price: {paintings.price} credits</p>
+    <button type="button">Add to basket </button>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(template);
+ReactDOM.render(template, document.getElementById("root"));
