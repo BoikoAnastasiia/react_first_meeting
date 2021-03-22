c206cdadc4e8399a0107ab2928d3eff0602c7ef0
anastasyah86 <nastasyah86@gmail.com>
2021-03-22 17:31:39 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React from "react";
 import Logo from "./components/logo";
 import PaintingList from "./components/PaintingList";
 import srcPaintings from "./src.json";
+import Panel from "./components/panel";
 
 const App = () => {
   return (
     <div>
+      <Panel title="fresh news">
+        <p>
+          Take two powerhouse women with impeccable comedic timing, add some
+          bad-ass superhero costumes, and that's really all we need to know.
+          This action comedy starring Melissa McCarthy and Octavia Spencer is
+          helmed by McCarthy's husband, Ben Falcone, and it might just provide
+          the heroes we've all been waiting for - even if their crime-fighting
+          quest was largely accidental.
+        </p>
+        <a href="https://app.schoology.com/calendar/93611783/2021-03"> Read</a>
+      </Panel>
+      <Panel title="fresh news">
+        <p>
+          What a long and storied history this release has had, from director
+          Zack Snyder's struggles with his studio, personal tragedy, his
+          replacement's fall from grace, and the subsequent rallying cry from
+          fans to #ReleasetheSnyderCut. Whether it's a trainwreck or a
+          triumphant closing chapter to this Hollywood story, make an extra
+          batch of popcorn for this 4-hour (!) event.
+        </p>
+        <a href="https://app.schoology.com/calendar/93611783/2021-03"> Read</a>
+      </Panel>
+
       <Logo text="The main component of an painting App container" />
       <PaintingList items={srcPaintings} />
     </div>
---
+++ src/components/panel.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+const styles = {
+    color: "green",
+};
+
+const Panel = ({ title, children }) => (
+    <div style={styles}>
+    {title && <h2>{title}</h2>}
+        {children}
+    </div>)
+
+Panel.defaultProps = {
+    title: "",
+    children: []
+    }
+
+Panel.propTypes = {
+    title: PropTypes.string,
+children: PropTypes.node,}
+export default Panel
