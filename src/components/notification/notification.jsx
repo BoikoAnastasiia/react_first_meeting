91230aaa496ed09b0fa949f1bbdcaac3385bb12b
anastasyah86 <nastasyah86@gmail.com>
2021-03-26 18:20:27 +0200

    work

--- src/components/notification/notification.jsx
+++ src/components/notification/notification.jsx
@@ -1,9 +1,9 @@
 import styles from './notification.module.css';
 import PropTypes from 'prop-types';
 
-const Notification = ({ text, type = 'success' }) => (
-  <p className={styles[type]}>{text}</p>
-);
+const Notification = ({ text, type }) => {
+  return <p className={styles[type]}>{text}</p>;
+};
 
 Notification.propTypes = {
   text: PropTypes.string.isRequired,
